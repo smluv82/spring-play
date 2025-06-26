dc031defbfabec6bc371ff9eca7eeeff5330f55e
steven <smluv82@naver.com>
2025-06-26 19:08:46 +0900

    [add] Redis Hashes, ZSet 관련 코드 추가

--- domain-play/src/main/kotlin/me/play/domain/dto/redis/RedisDtos.kt
+++ domain-play/src/main/kotlin/me/play/domain/dto/redis/RedisDtos.kt
@@ -1,24 +1,31 @@
 package me.play.domain.dto.redis
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo
+import io.swagger.v3.oas.annotations.media.Schema
 import java.io.Serial
 import java.io.Serializable
 import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
+import java.util.concurrent.TimeUnit
 
+@Schema(description = "Redis String request model")
 data class RedisStringDto(
     val key: String,
     val value: String,
-    val ttl: Long
+    val ttl: Long,
+    val timeUnit: TimeUnit = TimeUnit.MINUTES
 )
 
 
+@Schema(description = "Redis Lists 저장 request model")
 data class RedisListDto(
     val key: String,
     val values: List<String>,
     val isLeft: Boolean
 )
 
+
+@Schema(description = "Redis Lists 조회 request model")
 data class RedisListGetDto(
     val key: String,
     val count: Long? = null,
@@ -26,6 +33,53 @@ data class RedisListGetDto(
 )
 
 
+@Schema(description = "Redis Hash request model")
+data class RedisHashDto(
+    val key: String,
+    val hashKeyToHashValue: Map<String, String>
+)
+
+
+@Schema(description = "Redis ZSet request model")
+data class RedisZSetDto(
+    val key: String,
+    val value: String,
+    val score: Double
+)
+
+
+@Schema(description = "Redis ZSet 순위 조회 request model")
+data class RedisZSetRankDto(
+    val key: String,
+    val value: String,
+    val reverse: Boolean
+)
+
+
+@Schema(description = "Redis ZSet 범위 조회 request model")
+data class RedisZSetRangeDto(
+    val key: String,
+    val start: Long,
+    val end: Long,
+    val reverse: Boolean
+)
+
+
+enum class ZSetRemoveType {
+    VALUE, RANGE, SCORE
+}
+
+@Schema(description = "Redis ZSet 삭제 request model, RANGE인 경우 start, end는 Long, SCORE인 경우 Double형으로 인식")
+data class RedisZSetRemoveDto(
+    val key: String,
+    val removeType: ZSetRemoveType,
+    val value: String?,
+    val start: Number?,
+    val end: Number?
+)
+
+
+@Schema(description = "Redis TTL request model")
 data class RedisExpireDto(
     val key: String,
     val ttl: Long,
@@ -33,12 +87,15 @@ data class RedisExpireDto(
 )
 
 
+@Schema(description = "Redis Any 관련 request model")
 data class RedisAnyDto(
     val key: String,
     val value: RedisValueDto,
     val ttl: Long
 )
 
+
+@Schema(description = "Redis Any의 Value request model (Json Test)")
 @JsonTypeInfo(use = JsonTypeInfo.Id.CLASS, include = JsonTypeInfo.As.PROPERTY)
 data class RedisValueDto(
     val name: String,
--- domain-play/src/test/kotlin/me/play/domain/service/redis/RedisServiceTest.kt
+++ domain-play/src/test/kotlin/me/play/domain/service/redis/RedisServiceTest.kt
@@ -50,6 +50,35 @@ class RedisServiceTest {
     }
 
     @Test
+    @DisplayName("redis bulk set test")
+    fun redisBulkSetTest() {
+        log.info { "redis bulk set test" }
+        val reqDtos = listOf(
+            RedisStringDto(
+                key = "testKey1",
+                value = "testValue1",
+                ttl = 10L,
+                timeUnit = TimeUnit.MINUTES
+            ),
+            RedisStringDto(
+                key = "testKey2",
+                value = "testValue2",
+                ttl = 10L,
+                timeUnit = TimeUnit.MINUTES
+            ),
+            RedisStringDto(
+                key = "testKey3",
+                value = "testValue3",
+                ttl = 10L,
+                timeUnit = TimeUnit.MINUTES
+            )
+        )
+
+        redisService.saveByBulk(reqDtos)
+
+    }
+
+    @Test
     @DisplayName("redis string increment & decrement test")
     fun redisStringIncrementDecrementTest() {
         val key = "testIncrDecrKey"
@@ -99,7 +128,71 @@ class RedisServiceTest {
 
         //위에서 Pop을 하였으므로 Size 0이어야 함.
         val size = redisService.getByListSize(key)
-        assertThat(size).isEqualTo("0")
+        assertThat(size).isEqualTo(0L)
+    }
+
+
+    @Test
+    @DisplayName("hashes 관련 test")
+    fun hashesTest() {
+        log.info { "hashes test" }
+        val key = "testHashKey"
+        val hashKeyToHashValue = mapOf(
+            "name" to "steven",
+            "age" to "39",
+            "country" to "korea"
+        )
+
+        val reqDto = RedisHashDto(key, hashKeyToHashValue)
+
+        redisService.saveByHash(reqDto)
+
+        val hashKeys = redisService.getByKeys(key)
+        assertThat(hashKeys).isNotNull()
+        assertThat(hashKeys.containsAll(setOf("name", "age", "country"))).isTrue()
+        val hashValues = redisService.getByValues(key)
+        assertThat(hashValues).isNotNull()
+        assertThat(hashValues.containsAll(listOf("steven", "39", "korea"))).isTrue()
+    }
+
+
+
+    @Test
+    @DisplayName("ZSet 관련 test")
+    fun zSetTest() {
+        log.info { "zSet test" }
+        val key = "testZSetKey"
+
+        val valueToScore = mapOf(
+            "steven" to 1.0,
+            "lina" to 4.0,
+            "derek" to 2.0,
+            "peter" to 3.0
+        )
+
+        valueToScore.forEach { (name, score) ->
+            redisService.saveByZSet(RedisZSetDto(key, name, score))
+        }
+
+        var result = redisService.getByZSetRank(RedisZSetRankDto(key, "steven", false))
+        assertThat(result).isEqualTo(0)
+        result = redisService.getByZSetRank(RedisZSetRankDto(key, "steven", true))
+        assertThat(result).isEqualTo(3)
+
+
+        val result2 = redisService.getByZSetRange(RedisZSetRangeDto(key, 0, -1, false))
+        assertThat(result2).isNotNull()
+        result2?.forEachIndexed { index, s ->
+            if (index == 0)
+                assertThat(s).isEqualTo("steven")
+            else if (index == 1)
+                assertThat(s).isEqualTo("derek")
+            else if (index == 2)
+                assertThat(s).isEqualTo("peter")
+            else if (index == 3)
+                assertThat(s).isEqualTo("lina")
+        }
+
     }
 
 
