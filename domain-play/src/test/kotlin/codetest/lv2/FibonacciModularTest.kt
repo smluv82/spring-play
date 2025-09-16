4c237620e6704e1007f5ffeb6a268ff22758c2c3
steven <smluv82@naver.com>
2025-09-16 19:39:55 +0900

    [add] CodeTest TC

---
+++ domain-play/src/test/kotlin/codetest/lv2/FibonacciModularTest.kt
@@ -0,0 +1,39 @@
+package codetest.lv2
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class FibonacciModularTest {
+
+
+    fun solution(n: Int): Int {
+        val divide = 1234567
+        var before = 0
+        var current = 1
+
+        for (i in 2..n) {
+            val next = (before + current) % divide
+            before = current
+            current = next
+        }
+
+        return current
+    }
+
+    @Test
+    fun test1() {
+        val n = 3
+        val result = solution(n)
+        println(result)
+        assertThat(result).isEqualTo(2)
+    }
+
+
+    @Test
+    fun test2() {
+        val n = 5
+        val result = solution(n)
+        println(result)
+        assertThat(result).isEqualTo(5)
+    }
+}
---
+++ domain-play/src/test/kotlin/codetest/lv3/NetworkSumTest.kt
@@ -0,0 +1,58 @@
+package codetest.lv3
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+/**
+ * https://school.programmers.co.kr/learn/courses/30/lessons/43162
+ */
+class NetworkSumTest {
+
+    fun solution(n: Int, computers: Array<IntArray>): Int {
+        var networkCount = 0
+        val checked = BooleanArray(n) { false }
+
+        fun dfs(computer: Int) {
+            checked[computer] = true
+            for (anotherComputer in 0 until n) {
+                if (computers[computer][anotherComputer] == 1 && !checked[anotherComputer]) {
+                    dfs(anotherComputer)
+                }
+            }
+        }
+
+        for (i in 0 until n) {
+            if (!checked[i]) {
+                dfs(i)
+                networkCount++
+            }
+        }
+
+        return networkCount
+    }
+
+
+    @Test
+    fun test1() {
+        val computerCount = 3
+        val computers = arrayOf(
+            intArrayOf(1, 1, 0),
+            intArrayOf(1, 1, 0),
+            intArrayOf(0, 0, 1)
+        )
+        val result = solution(computerCount, computers)
+        assertThat(result).isEqualTo(2)
+    }
+
+    @Test
+    fun test2() {
+        val computerCount = 1
+        val computers = arrayOf(
+            intArrayOf(1, 1, 0),
+            intArrayOf(1, 1, 1),
+            intArrayOf(0, 1, 1)
+        )
+        val result = solution(computerCount, computers)
+        assertThat(result).isEqualTo(1)
+    }
+}
