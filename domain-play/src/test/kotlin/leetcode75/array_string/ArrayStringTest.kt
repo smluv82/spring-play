bdfd35372eaabe2512699a3f243110aed23613eb
steven <smluv82@naver.com>
2025-09-23 19:42:12 +0900

    [code-test] leetcode75 - 2번째 문제

--- domain-play/src/test/kotlin/leetcode75/array_string/ArrayStringTest.kt
+++ domain-play/src/test/kotlin/leetcode75/array_string/ArrayStringTest.kt
@@ -57,3 +57,55 @@ class MergeStringsAlternatelyTest {
         assertThat(result).isEqualTo("apbqcd")
     }
 }
+
+
+/**
+ * https://leetcode.com/problems/greatest-common-divisor-of-strings/description/?envType=study-plan-v2&envId=leetcode-75
+ */
+class GreatestCommonDivisorOfStringsTest {
+    fun solution(str1: String, str2: String): String {
+        // 앞뒤가 같은지 먼저 체크, 앞뒤로 안맞으면 틀림.
+        if (str1 + str2 != str2 + str1) {
+            return ""
+        }
+        val gcd = getGCD(str1.length, str2.length)
+
+        return str2.substring(0, gcd)
+    }
+
+    private tailrec fun getGCD(a: Int, b: Int): Int {
+        if (b == 0)
+            return a
+        return getGCD(b, a % b)
+    }
+
+    @Test
+    fun test1() {
+        val str1 = "ABCABC"
+        val str2 = "ABC"
+
+        val result = solution(str1, str2)
+
+        assertThat(result).isEqualTo("ABC")
+    }
+
+    @Test
+    fun test2() {
+        val str1 = "ABABAB"
+        val str2 = "ABAB"
+
+        val result = solution(str1, str2)
+
+        assertThat(result).isEqualTo("AB")
+    }
+
+    @Test
+    fun test3() {
+        val str1 = "LEET"
+        val str2 = "CODE"
+
+        val result = solution(str1, str2)
+
+        assertThat(result).isEqualTo("")
+    }
+}
